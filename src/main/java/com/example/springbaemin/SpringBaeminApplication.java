5fb81c9f3e879f25bafecc719aee05618bc54850
sunyoung <4446207@naver.com>
2024-05-07 08:34:55 +0900

    feat: first commit

---
+++ src/main/java/com/example/springbaemin/SpringBaeminApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbaemin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBaeminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBaeminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbaemin/SpringBaeminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbaemin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBaeminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
